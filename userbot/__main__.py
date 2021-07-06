3e4ea0d2e192509d20a20d3292d64928d5a7cc2d
Teguh <86183102+kenzx23@users.noreply.github.com>
2021-07-06 10:01:53 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lord-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"KEN-UBOT ⚙️ V{BOT_VER} [DAH AKTIF GOBLOK!]")
 
 
 if len(argv) not in (1, 3, 4):
