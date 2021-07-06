a52901a23d537dddc98269198edb3761849f76a0
Ken <86183102+kenzx23@users.noreply.github.com>
2021-07-06 18:29:27 +0700

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -1543,7 +1543,7 @@ async def typewriter(typew):
 @register(outgoing=True, pattern=r"^\.leave$")
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`Lord Telah Meninggalkan Grup ツ`")
+        await e.edit("`Gc jelek najis cuihhh mending gua out ajalah, bye anak kontol!!!`")
 
 
 @register(outgoing=True, pattern=r"^\.fail$")
